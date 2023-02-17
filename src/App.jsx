8ad7384729cc16e78805a37c870ad6cab78e3b62
t31ly <woraponklabsri7731@gmail.com>
2023-02-18 00:14:05 +0700

    constructor

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="bg-[#212529]">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav className='h-[60px] w-[100%] border-2'>
+        
+    </nav>
+  )
+}
+
+export default Header
