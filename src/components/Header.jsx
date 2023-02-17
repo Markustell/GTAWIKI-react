1ba51488b013b75d2c5195a2717ff8831561956c
t31ly <woraponklabsri7731@gmail.com>
2023-02-18 01:45:31 +0700

    [Header.jsx] updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,14 @@ function Header() {
   return (
     <header className='flex justify-center h-[80px] w-[100%]'>
       <nav className='flex justify-between items-center w-[90%] border-b-[1px]'>
-        <div className="flex h-full w-1/2 items-center border-[1px]">
+        <div className="flex h-full w-1/2 items-center">
           <img src="/src/images/logo/short_logo.png" alt="GTAWIKI" className='lg:hidden h-[60px]'/>
           <img src="/src/images/logo/horizontal_logo.png" alt="GTAWIKI" className='hidden lg:block h-[60px]'/>
         </div>
-        <div className="flex items-center justify-end border-[1px] h-full w-1/2">
-          <form action="">
-            <IonIcon icon={search} className='text-white mx-2 translate-y-[2px]'/>
-            <input type="text" />
+        <div className="flex items-center justify-end h-full w-1/2 ">
+          <form action="" className='w-full lg:w-5/6'>
+            <IonIcon icon={search} className='text-black ml-3 translate-y-[9px] absolute'/>
+            <input type="text" className='w-full h-[35px] pl-9 rounded-full'/>
           </form>
         </div>
       </nav>
