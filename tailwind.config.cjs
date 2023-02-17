919d24d85442c359fd2a53ba2ef0c91d6f8a486d
t31ly <woraponklabsri7731@gmail.com>
2023-02-18 04:12:15 +0700

    updated background animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,9 +8,9 @@ module.exports = {
     extend: {
       keyframes: {
         zoomloop: {
-          '0%': { backgroundSize: '150%'},
-          '50%': { backgroundSize: '160%'},
-          '100%': { backgroundSize: '150%'},
+          '0%': { backgroundSize: '200%'},
+          '50%': { backgroundSize: '210%'},
+          '100%': { backgroundSize: '200%'},
         }
       },
     },
